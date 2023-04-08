5488faabdaaece3208466aeda15968edccc84737
Looyyd <filip.a.niedzielski@gmail.com>
2023-04-08 16:34:17 +0200

    basic upload works

--- libs/upload_podcast.py
+++ libs/upload_podcast.py
@@ -23,6 +23,27 @@ def upload_audio(upload_url, file_path):
 
     return response
 
+# create a new episode
+def create_episode(show_id, title, summary, audio_url, number=1, description=""):
+    url = "https://api.transistor.fm/v1/episodes"
+    headers = {"x-api-key": TRANSISTOR_API_KEY}
+    data = {
+        "episode[show_id]": show_id,
+        "episode[title]": title,
+        "episode[summary]": summary,
+        "episode[number]": number,
+        "episode[description]": description,
+        "episode[audio_url]": audio_url
+    }
+    
+    response = requests.post(url, headers=headers, data=data)
+    
+    if response.status_code == 201:
+        return response.json()
+    else:
+        raise ValueError(f"Failed to create episode, status code: {response.status_code}, message: {response.text}")
+
+
 
 if __name__ == '__main__':
     response = authorize_upload('test.mp3')
@@ -32,5 +53,16 @@ if __name__ == '__main__':
     audio_url = json_response['data']['attributes']['audio_url']
     print(upload_url)
     print(audio_url)
+
+    # TODO: add it to function
+    podcast_number = 1
+    file_path = "podcast/podcast{}.mp3".format(podcast_number)
+    response = upload_audio(upload_url, file_path)
+    print(response)
+
+    show_id = str(40581)
+    response = create_episode(show_id, "test", "test", audio_url, number=podcast_number)
+    print(response)
+
 
 
